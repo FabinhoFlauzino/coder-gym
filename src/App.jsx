b6edb4fd154293c13066aa69225223f80669b911
Fabio Flauzino <fabio@mfrural.com.br>
2024-09-09 12:04:29 -0300

    seção banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,23 @@ import { Hero } from "./components/Hero"
 import { Navbar } from "./components/Navbar"
 import BgImage from "./assets/bg.png"
 import { Equipaments } from "./components/Equipaments"
+import { Banner } from "./components/Banner"
+import Img1 from "./assets/2.png"
+import Img2 from "./assets/3.png"
+
+const BannerData = {
+  image: Img1,
+  title: `The Importants To Take Care Of Yourself`,
+  subtitle: `The Importants To Take Care Of Yourself Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nam obcaecati sequi odio dolore officia magni reiciendis, architecto, eos, aut asperiores reprehenderit quas rerum omnis facilis quam eius doloribus maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae vero inventore assumenda ducimus alias doloribus.`,
+  link: "#"
+}
+
+const Banner2Data = {
+  image: Img2,
+  title: `The Importants To Take Care Of Yourself`,
+  subtitle: `The Importants To Take Care Of Yourself Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nam obcaecati sequi odio dolore officia magni reiciendis, architecto, eos, aut asperiores reprehenderit quas rerum omnis facilis quam eius doloribus maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae vero inventore assumenda ducimus alias doloribus.`,
+  link: "#"
+}
 
 const bgStyle = {
   backgroundImage: `url(${BgImage})`,
@@ -19,6 +36,9 @@ function App() {
         <Hero />
       </div>
       <Equipaments />
+      <Banner {...BannerData}/>
+      
+      <Banner {...Banner2Data}/>
     </div>
   )
 }
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,5 +1,7 @@
 import { FaPlay } from "react-icons/fa";
- import HeroImg from "../../assets/dumbell.png"
+import HeroImg from "../../assets/dumbell.png";
+import { motion } from "framer-motion";
+import { SlideLeft, SlideRight } from "../../utility/animation";
 
 export function Hero() {
   return (
@@ -8,22 +10,50 @@ export function Hero() {
         <div className="container grid grid-cols-1 lg:grid-cols-2 min-h-[650px] relative">
           <div className="flex flex-col justify-center py-14 lg:py-0 font-playfair">
             <div className="text-center lg:text-left space-y-6">
-              <h1 className="text-5xl lg:text-6xl font-bold leading-relaxed xl:leading-normal">Gym Gives you the perfect <span className="text-primary">Health</span></h1>
-              <p className="text-gray-600 xl:max-w-[768px]">It is a long established fact that a reader will be by readable content of a page when are the best product.</p>
-              <div className="flex justify-center gap-8 lg:justify-start !mt-4">
+              <motion.h1
+                variants={SlideRight(0.6)}
+                initial="hidden"
+                animate="visible"
+                className="text-5xl lg:text-6xl font-bold leading-relaxed xl:leading-normal"
+              >
+                Gym Gives you the perfect{" "}
+                <span className="text-primary">Health</span>
+              </motion.h1>
+              <motion.p
+                variants={SlideRight(0.9)}
+                initial="hidden"
+                animate="visible"
+                className="text-gray-600 xl:max-w-[500px]"
+              >
+                It is a long established fact that a reader will be by readable
+                content of a page when are the best product.
+              </motion.p>
+              <motion.div
+                variants={SlideRight(1.2)}
+                initial="hidden"
+                animate="visible"
+                className="flex justify-center gap-8 lg:justify-start !mt-4"
+              >
                 <button className="primary-btn">Order Now</button>
                 <button className="flex justify-center items-center gap-2">
                   <FaPlay /> Watch Now
                 </button>
-              </div>
+              </motion.div>
             </div>
           </div>
 
           <div className="flex items-center justify-center">
-            <img src={HeroImg} alt="Imagem Hero" className="w-[350px] lg:w-[700px] drop-shadow"/>
+            <motion.img
+              initial={{ opacity: 0, x: 100, rotate: 400 }}
+              animate={{ opacity: 1, x: 0, rotate: 360 }}
+              transition={{ duration: 0.7, ease: "linear", delay: 0.9 }}
+              src={HeroImg}
+              alt="Imagem Hero"
+              className="w-[350px] lg:w-[700px] drop-shadow"
+            />
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
